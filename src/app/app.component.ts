115bd53afce29311f28fe5bee719dde461f8568f
abhishekH1992 <abhishekhonrao103@gmail.com>
2025-05-01 13:00:01 +1200

    Router outlet for routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
-import { OverviewComponent } from './overview/overview.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
-    imports: [HeaderComponent, OverviewComponent],
+    imports: [RouterOutlet, HeaderComponent],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+    styleUrl: './app.component.css',
+    standalone: true
 })
 export class AppComponent {
     title = 'phq-bookmark-manager-test';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { OverviewComponent } from './overview/overview.component';
+import { ResultComponent } from './result/result.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        loadComponent: () => import('./overview/overview.component').then(m => m.OverviewComponent)
+    },
+    {
+        path: 'result',
+        loadComponent: () => import('./result/result.component').then(m => m.ResultComponent)
+    }
+];
